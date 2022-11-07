46b58cee7688e3a09e59406130b48f3325167907
xsilvamo <aaronsilva.csl@gmail.com>
2022-11-07 03:26:56 -0300

    modificacion de analisis

--- analisisM.py
+++ analisisM.py
@@ -1,6 +1,7 @@
 import armaduras
 import numpy as np
 
+#creamos la clase de analisis de la matriz
 class AnalisisMatricial():
     def __init__(self, tabla_elem, tabla_nodos, tabla_fuerzas, tabla_desplazamientos):
         self.tE = tabla_elem
@@ -9,7 +10,7 @@ class AnalisisMatricial():
         self.tD = tabla_desplazamientos
 
 
-
+#creamos las tablas en funcion del usuario
 tabla_elem = [
     ['E1', 1, 1, 'N5', 'N1'],
     ['E2', 1, 1, 'N2', 'N1'],
@@ -18,7 +19,6 @@ tabla_elem = [
     ['E5', 1, 1, 'N3', 'N5']
     ['E6', 1, 1, 'N4', 'N5'],
     ['E7', 1, 1, 'N4', 'N3']]
-    
 
 tabla_nodos = []
 tabla_fuerzas = []
